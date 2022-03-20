27125b53509220ea9cefea0336bd955f2f39f3c6
vitdias <55774398+vitdias@users.noreply.github.com>
2022-03-20 20:08:11 -0300

    Revert "Revert "Initial commit""
    This reverts commit 1a28a073254fbade4e47f05a87b5f71eba397594.

---
+++ AccessPhoneWebcam.py
@@ -0,0 +1,22 @@
+import requests
+import cv2
+import numpy as np
+import imutils
+import socket
+
+# Get the URL by download app IP Webcam in play store. Then make sure that your PC and your phone are in the same wifi network.
+# In IP Webcam app, click in "Start Server".
+# To this code work, you need to maintain ther server running
+
+url = "http://192.168.0.249:8080/shot.jpg"
+
+while True:
+    img_resp = requests.get(url)
+    img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+    img = cv2.imdecode(img_arr, -1)
+    img = imutils.resize(img, width=1000, height=1000)
+    cv2.imshow("VIT_android_cam", img)
+
+    #Press Esc to exit
+    if cv2.waitKey(1) == 27:
+        break
---
+++ README.md
@@ -0,0 +1,2 @@
+# Cel-as-Webcam
+ Use the camera in your mobile phone as webcam and stream it in your pc
